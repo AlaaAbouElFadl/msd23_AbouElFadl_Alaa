e28b5f7d955c0f6a2ed121bc8f938e745ef5fa49
Alaa Abou El Fadl <alaa.abouelfadl@edu.fh-joanneum.at>
2023-05-16 13:18:58 +0200

    < src/main/java/at/fhj/msd/Calculator added to the repository>

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,5 +1,16 @@
-package org.example;
+package at.fhj.msd;
 
 public class Calculator {
-
+    public double add(double num1, double num2){
+        return num1 + num2;
+    }
+    public double minus(double num1, double num2){
+        return num1 - num2;
+    }
+    public double divide(double num1, double num2){
+        return num1 / num2;
+    }
+    public double multiply(double num1, double num2){
+        return num1 * num2;
+    }
 }
--- src/main/java/at/fhj/msd/Main.java
+++ src/main/java/at/fhj/msd/Main.java
@@ -1,7 +1,22 @@
-package org.example;
+package at.fhj.msd;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calculator = new Calculator();
+
+        double num1 = 7.0;
+        double num2 = 3.0;
+
+        double addResult = calculator.add(num1, num2);
+        System.out.println(num1 + " + " + num2 + " = " + addResult);
+
+        double subtractResult = calculator.minus(num1, num2);
+        System.out.println(num1 + " - " + num2 + " = " + subtractResult);
+
+        double multiplyResult = calculator.multiply(num1, num2);
+        System.out.println(num1 + " * " + num2 + " = " + multiplyResult);
+
+        double divideResult = calculator.divide(num1, num2);
+        System.out.println(num1 + " / " + num2 + " = " + divideResult);
     }
 }
